b7542d9b272953247445f2eb04643d35f587c71c
KaveenAshara <IT22120816@my.sliit.lk>
2024-05-12 12:21:11 +0530

    Implementing NoteViewModel class

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,20 +1,102 @@
 package com.example.notes
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notes.Adapter.NotesAdapter
+import com.example.notes.Database.NoteDatabase
+import com.example.notes.Models.Note
+import com.example.notes.Models.NoteViewModel
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel :NoteViewModel
+    lateinit var adapter:NotesAdapter
+    lateinit var selectedNote:Note
+
+    private val updateNote =registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+
+        if (result.resultCode== Activity.RESULT_OK){
+
+            
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+
+        viewModel=ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+
+        viewModel.allnotes.observe(this) {list ->
+            list?.let{
+                adapter.updateList(list)
+            }
         }
+
+
+        database=NoteDatabase.getDatabase(this)
+    }
+
+    private fun initUI() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this,this)
+        binding.recyclerView.adapter=adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+
+            if (result.resultCode == Activity.RESULT_OK){
+
+                val note =result.data?.getSerializableExtra("note") as? Note
+                if(note !=null){
+
+                    viewModel.insertNote(note)
+
+                }
+
+            }
+
+        }
+
+        binding.fbAddNote.setOnClickListener{
+
+            val intent = Intent(this,AddNote::class.java)
+
+            getContent.launch(intent)
+        }
+
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                if(newText !=null){
+
+                    adapter.filterList(newText)
+                }
+                return true
+            }
+
+
+        })
     }
 }
